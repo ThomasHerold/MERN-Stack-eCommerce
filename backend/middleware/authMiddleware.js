1f034d51cac18745c29252d492a554cff47c9353
Thomas Herold <thomas.herold95@gmail.com>
2021-01-24 09:45:46 -0500

    Add user registration route and password hashing

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -11,9 +11,15 @@ export const protect = asyncHandler(async (req, res, next) => {
     try {
       token = req.headers.authorization.split(' ')[1];
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      console.log(decoded);
+
+      req.user = await User.findById(decoded.id).select('-password');
+
       next();
-    } catch (err) {}
+    } catch (err) {
+      console.error(err);
+      res.status(401);
+      throw new Error('Not authorized, token failed');
+    }
   }
 
   if (!token) {
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -33,6 +33,16 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
+// check if password has been created/modified before saving to DB, then encrypt if true
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
 const User = mongoose.model('User', userSchema);
 
 export default User;
